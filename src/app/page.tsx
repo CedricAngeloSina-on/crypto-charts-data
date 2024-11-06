436dd19c22ba52c9fd1c1b143d6b0d46b8ef1a64
CedricAngeloSina-on <cao.sinaon@gmail.com>
2024-11-06 23:33:19 +0800

    feat: crypto chart card layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import { Card, CardContent, CardHeader } from "~/components/ui/card";
 import { ThemeTrigger } from "~/components/theme-switcher";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <ThemeTrigger />
+    <main className="flex min-h-screen flex-col items-center justify-center px-4">
+      <div className="absolute left-4 top-4">
+        <ThemeTrigger />
+      </div>
+      <Card className="w-full max-w-screen-lg">
+        <CardHeader className="flex flex-row items-start justify-between space-y-0 px-4 py-5 sm:py-6 md:px-6">
+          <div className="flex flex-1 flex-col justify-center gap-1">
+            {/* CRPYTO SELECTOR */}
+          </div>
+        </CardHeader>
+        <CardContent className="w-full px-2 sm:p-6">
+          <div className="flex aspect-auto h-[400px] w-full flex-col items-center justify-center">
+            {/* CHART COMPONENT */}
+          </div>
+        </CardContent>
+      </Card>
     </main>
   );
 }
